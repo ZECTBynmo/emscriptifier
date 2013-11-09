f65a72ef0581d1175f9b7e9929341ab89a72143d
ZECTBynmo <michael.vegeto@gmail.com>
2013-11-08 19:51:37 -0500

    Add an ace editor to the home page

--- public/js/views/home.js
+++ public/js/views/home.js
@@ -2,10 +2,19 @@ window.HomeView = Backbone.View.extend({
 
     initialize:function () {
         this.render();
+
+        setTimeout(function() {
+	        var editor = ace.edit("editor");
+			editor.setTheme("ace/theme/monokai");
+			editor.getSession().setMode("ace/mode/javascript");
+
+			$('#editor').height($(window).height());
+	    }, 0);
     },
 
     render:function () {
         $(this.el).html(this.template());
+
         return this;
     }
 
